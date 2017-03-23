0f7aa05fdc962ee10ccfb57069ea90f607abcc99
Hao8816 <chenhao8816+8816@gmail.com>
2017-03-23 21:28:17 +0800

    fixed bug

--- chat_box/js/chat.js
+++ chat_box/js/chat.js
@@ -121,7 +121,7 @@ angular.module('chat').controller(
             }
         ];
 
-        socket.emit('recently_list',{'uid':'12'});
+        socket.emit('recently_list',{'uid':$rootScope.uid});
         socket.on('recently_list', function(data){
             console.log("发送回去最近聊天列表...");
         });
@@ -174,7 +174,7 @@ angular.module('chat').controller(
         $rootScope.link_index = 2;
         $scope.contact_index = 2;
 
-        socket.emit('contact_list',{'uid':'12'});
+        socket.emit('contact_list',{'uid':$rootScope.uid});
         socket.on('contact_list', function(data){
             console.log("请求好友列表。。。",data);
         });
